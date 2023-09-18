b8607de5cf92c28a6804b485c37c996101142161
Justin Lee <justinslee201100@yahoo.com>
2023-09-18 11:24:26 -0600

    kind of make last 2 methods in iteration 3

--- lib/market.rb
+++ lib/market.rb
@@ -24,11 +24,26 @@ class Market
 
   def total_inventory
     total_inventory = Hash.new(0)
+    # require "pry"; binding.pry
     @vendors.each do |vendor|
       vendor.inventory.each do |item, quantity|
-        total_inventory[item] += quantity
+        total_inventory[item] += quantity.to_i
       end
     end
     total_inventory
+  end
+
+  def sorted_item_list
+    total_inventory.keys.map do |item|
+      item.name
+    end.sort.uniq
+  end
+
+  def overstocked_items
+    total_inventory.select do |item, quantity|
+      if quantity > 50 && vendors_that_sell(item).length > 1
+        item
+      end
+    end
   end
 end
